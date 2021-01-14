1b5aaa0259d9b83d8b6a10f96b7c3a65080f1215
Angel <calvo.monzon.angel@gmail.com>
2021-01-14 15:56:38 +0100

    avances

--- movements/views.py
+++ movements/views.py
@@ -4,13 +4,51 @@ import sqlite3
 
 DB_FILE = app.config["DB_FILE"]
 
+def consulta(query, params =()):
+    
+    #CONECTAMOS CON LA BASE DE DATOS
+    conn = sqlite3.connect(DB_FILE)
+    #CREAR EL CURSOS
+    c = conn.cursor() 
+    #INSERTAR LOS DATOS ESTABLECIDOS EN EL DATA BASE
+    c.execute(query, params)
+    #SALVAR LOS CAMBIOS
+    conn.commit()
+    #RECUPERAMOS LAS FILAS
+    filas = c.fetchall()
+    #CERRAMOS LA CONEXION
+    conn.close()
+    
+    if len(filas) == 0:
+        return filas
+    
+    nombreColumnas = []
+    for nombreColumna in c.description: #c.description proporciona los nombres de columna de la última consulta
+        nombreColumnas.append(nombreColumna[0]) #añadimos la posición 0 de las columnas que hemos consultado
+        
+    diccionarios_consulta = []
+    
+    for fila in filas:
+        d = {}
+        for ix, nombreColumna in enumerate(nombreColumnas):
+            d[nombreColumna] = fila[ix]
+        diccionarios_consulta.append(d)
+        
+    return diccionarios_consulta
+
 @app.route('/')
 def index():
+    
+    operaciones = consulta("SELECT ")
+    
     return render_template ("index.html")
 
-@app.route("/purchase")
+@app.route("/purchase", methods=["GET", "POST"])
 def compra_venta():
-    return "compra y venta de criptomonedas"
+    if request.method == "POST":
+        request.form[]
+        
+        return redirect(url_for("index"))
 
 @app.route("/status")
 def estado_inversion():
