42e000c942fd702980e53da46e93ddc88f294fe2
Angel <calvo.monzon.angel@gmail.com>
2021-01-14 11:43:21 +0100

    cambios proyecto

--- movements/views.py
+++ movements/views.py
@@ -1,5 +1,17 @@
 from flask import render_template, request, url_for, redirect
 from movements import app
+import sqlite3
+
+DB_FILE = app.config["DB_FILE"]
+
 @app.route('/')
 def index():
     return "Inicio"
+
+@app.route("/purchase")
+def compra_venta():
+    return "compra y venta de criptomonedas"
+
+@app.route("/status")
+def estado_inversion():
+    return "estado de las inversiones"
