6efdf1321088a9a7a58b83064dffb27e2445db17
Angel <calvo.monzon.angel@gmail.com>
2021-01-24 18:52:16 +0100

    cambios

--- movements/cambio.py
+++ movements/cambio.py
@@ -1,26 +1,25 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+from movements.validacion import Validacion
 
 API_KEY = '8c547a52-a34e-4549-9dd2-90fe0266d0af'
 criptomonedas = ('BTC')
 
-for moneda in criptomonedas:
-  url = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
-  headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': '8c547a52-a34e-4549-9dd2-90fe0266d0af',}
-  parameters = {
-    'symbol': moneda,
-    'amount': ,
-    'convert': ,
-    }
+url = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '8c547a52-a34e-4549-9dd2-90fe0266d0af',}
+parameters = {
+  'symbol': validacion.from_currency.data,
+  'amount': validacion.from_quantity.data, 
+  'convert': validacion.to_currency.data,}
   
-  session = Session()
-  session.headers.update(headers)
-  try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    print(data)
-  except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
+session = Session()
+session.headers.update(headers)
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
--- movements/validacion.py
+++ movements/validacion.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, DateField, StringField, FloatField, SubmitField, ValidationError
+from wtforms import IntegerField, DateField, StringField, FloatField, SubmitField, ValidationError, SelectField
 from wtforms.validators import DataRequired, Length
 from datetime import datetime, date, time
 import requests
@@ -7,8 +7,8 @@ import requests
 class Validacion(FlaskForm):
     date = datetime.now().strftime("%d/%m/%Y")
     time = datetime.now().strftime("%H:%M:%S")
-    from_currency = StringField ("from_currency", validators = [DataRequired()])
-    from_quantity = StringField('from_quantity', validators=[DataRequired()])
-    to_currency = StringField ("to_currency", validators = [DataRequired()])
-    calculadora = SubmitField("Convertir")
+    from_currency = SelectField ('from_currency', choices= [('BTC','BTC'),('ETH','ETH'),('XRP','XRP'),('LTC','LTC'),('BCH','BCH'),('BNB','BNB'),('USTD','USTD'),('EOS','EOS'),('BSV','BSV'),('XLM','XLM'),('ADA','ADA'),('TRX','TRX')], validators = [DataRequired()])
+    from_quantity = SelectField ('from_quantity', choices= [('BTC','BTC'),('ETH','ETH'),('XRP','XRP'),('LTC','LTC'),('BCH','BCH'),('BNB','BNB'),('USTD','USTD'),('EOS','EOS'),('BSV','BSV'),('XLM','XLM'),('ADA','ADA'),('TRX','TRX')], validators=[DataRequired()])
+    to_currency = FloatField ("to_currency", validators = [DataRequired()])
+    to_quantity = StringField('to_quantity')
     submit = SubmitField("Aceptar")
