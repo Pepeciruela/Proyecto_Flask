5b874350a0423731586f92b9fe07b67ddfe04567
Angel <calvo.monzon.angel@gmail.com>
2021-02-04 18:16:53 +0100

    cambios

--- movements/views.py
+++ movements/views.py
@@ -11,21 +11,42 @@ API_KEY = app.config["API_KEY"]
 
 @app.route('/')
 def index():
-    operaciones = consulta("SELECT date, time, from_currency, from_quantity, to_currency, to_quantity, p_u, id FROM criptomonedas;") #De la función consulta, llamamos a los parámteros que precisamos
-    
-    return render_template ("index.html", datos = operaciones, title = "Movimientos de compra y venta de Criptomonedas") #Devolver en pantalla la información solicitada
+    mensajes =[]
+    try: 
+        operaciones = consulta("SELECT date, time, from_currency, from_quantity, to_currency, to_quantity, p_u, id FROM criptomonedas;") #De la función consulta, llamamos a los parámteros que precisamos
+    except Exception as e:
+        print("**ERROR**: Acceso a base de datos - consulta registro operaciones: {} - {}". format(type(e).__name__, e))
+        mensajes.append("Error en acceso a base de datos. Consulte con el administrador.")  
+        return render_template ("index.html", datos = [], title = "Movimientos de compra y venta de Criptomonedas", mensajes=mensajes) #Devolver en pantalla la información solicitada
+    return render_template ("index.html", datos = operaciones, title = "Movimientos de compra y venta de Criptomonedas", mensajes=mensajes) #Devolver en pantalla la información solicitada
 
 @app.route("/purchase", methods=["GET", "POST"])
 def compra_venta():
+    mensajes =[]
+    validacion = Validacion()
     if request.method == 'GET': # 1º Validamos las monedas disponibles para comprar
-        validar_monedas = monedas_inicio()            
-        validacion = Validacion()
+        try: 
+            validar_monedas = monedas_inicio()
+        except Exception as e:
+            print("**ERROR**: Acceso a base de datos - consulta monedas disponibles al inicio: {} - {}". format(type(e).__name__, e))
+            mensajes.append("Error en acceso a base de datos. Consulte con el administrador.")           
+            return render_template('compra_criptos.html', validacion = validacion, validar_monedas=[], mensajes=mensajes)
+        
         validacion.from_currency.choices = validar_monedas
-
-        return render_template('compra_criptos.html', validacion = validacion, validar_monedas=validar_monedas)
+        
+        return render_template('compra_criptos.html', validacion = validacion, validar_monedas=validar_monedas, mensajes=mensajes)
     
     else: # Si el metodo es POST
-        validacion = Validacion()
+        try: 
+            validar_monedas = monedas_inicio()
+        except Exception as e:
+            print("**ERROR**: Acceso a base de datos - consulta monedas disponibles al inicio: {} - {}". format(type(e).__name__, e))
+            mensajes.append("Error en acceso a base de datos. Consulte con el administrador.")
+            return render_template('compra_criptos.html', validacion = validacion, validar_monedas=[], mensajes=mensajes)  
+        #validacion = Validacion()
+        validacion.from_currency.choices = validar_monedas
+       
+        
         if request.form.get('calculadora') == 'Calculadora' and validacion.validate: # Si le damos a la calculadora, llamamos a la API
             amount = request.form.get('from_quantity')
             symbol = request.form.get('from_currency')
@@ -36,8 +57,13 @@ def compra_venta():
                 
             comprobacion = dict(zip(valor1, valor2))
                 
-            #AQUI COMPROBAMOS QUE EL AMOUNT DISPONIBLE SEA EL QUE TENEMOS DENTRO DE RESERVA_MONEDAS
-            hucha = monedas_disponibles()
+            try: 
+                hucha = monedas_disponibles()
+            except Exception as e:
+                print("**ERROR**: Acceso a base de datos - validacion valor monedas: {} - {}". format(type(e).__name__, e))
+                mensajes.append("Error en acceso a base de datos. Consulte con el administrador.")
+                return render_template('compra_criptos.html', validacion = validacion, validar_monedas=[], mensajes=mensajes) 
+            
             monedas_saldo = {}
             for moneda in hucha:
                 if hucha[moneda] > 0:
@@ -46,7 +72,6 @@ def compra_venta():
             if symbol != convert:
                 lista_comprobacion = list(comprobacion.items())
                 lista_hucha = list(monedas_saldo.items())
-            #HACER ALGO PARA QUE SI NO HAY MONEDAS SUFICIENTES RETORNE UN MENSAJE Y DE ERROR
                 for clave in lista_comprobacion:
                     for otra_clave in lista_hucha:
                         if clave[0] == otra_clave[0]:
@@ -54,50 +79,66 @@ def compra_venta():
                                 print ("Hay dinero de sobra")
                                 try: 
                                     url = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={}&symbol={}&convert={}&CMC_PRO_API_KEY={}'.format(amount,symbol,convert,API_KEY)
-                                    respuesta = consulta_api(url) #Llamamos a la función de consuta de la API
-                                                    
+                                    try: 
+                                        respuesta = consulta_api(url) #Llamamos a la función de consuta de la API
+                                    except Exception as e:
+                                        print("**ERROR**: Acceso a la API - intercambio de precio de las monedas: {} - {}". format(type(e).__name__, e))
+                                        mensajes.append("Error en acceso a la API. Consulte con el administrador.")
+                                        return render_template('compra_criptos.html', validacion = validacion, validar_monedas=[], mensajes=mensajes) 
+                                               
                                     to_quantity = respuesta['data']['quote'][convert]['price'] # Sacamos el precio de conversión de la moneda a cambiar
                                                     
                                     p_u = float(amount)/to_quantity # Sacamos el precio unitario dividiendo entre la cantidad
                                                     
                                     return render_template ("compra_criptos.html", validacion = validacion, to_quantity = to_quantity, p_u = p_u) 
                                 except:
-                                    validar_monedas = monedas_inicio()
-                                    validacion = Validacion()
-                                    validacion.from_currency.choices = validar_monedas
                                     return render_template ('compra_criptos.html', validacion=validacion, validar_monedas = validar_monedas)
                             else:
                                 flash("No tienes suficientes monedas para realizar esta compra")
-                                validar_monedas = monedas_inicio()
-                                validacion = Validacion()
-                                validacion.from_currency.choices = validar_monedas
+                        
                                 return render_template ('compra_criptos.html', validacion=validacion, validar_monedas = validar_monedas)
-                
+            else:
+                flash("Las monedas deben ser diferentes")
+                return render_template ('compra_criptos.html', validacion=validacion, validar_monedas = validar_monedas)
+                    
         elif request.form.get('submit') == 'Aceptar' and validacion.validate:
-            
-            consulta("INSERT INTO criptomonedas(date, time, from_currency, from_quantity, to_currency, to_quantity, p_u) VALUES(?, ?, ?, ?, ?, ?, ?);", 
-                    (datetime.now().strftime("%d/%m/%Y"), 
-                    datetime.now().strftime("%H:%M:%S"), 
-                    request.form.get("from_currency"), 
-                    request.form.get("from_quantity"),
-                    request.form.get("to_currency"), 
-                    request.form.get("to_quantity"),
-                    request.form.get("p_u"),
-                    ))
-            return redirect(url_for("index")) #Si todos los datos están validados y son correctos, muestramelos en la función de inicio
-            #except:
-                #return render_template ('compra_criptos.html', validacion=validacion)           
+            try: 
+                consulta("INSERT INTO criptomonedas(date, time, from_currency, from_quantity, to_currency, to_quantity, p_u) VALUES(?, ?, ?, ?, ?, ?, ?);", 
+                        (datetime.now().strftime("%d/%m/%Y"), 
+                        datetime.now().strftime("%H:%M:%S"), 
+                        request.form.get("from_currency"), 
+                        request.form.get("from_quantity"),
+                        request.form.get("to_currency"), 
+                        request.form.get("to_quantity"),
+                        request.form.get("p_u"),
+                        ))
+                return redirect(url_for("index"))
+            except Exception as e:
+                print("**ERROR**: Acceso a la base de datos - introduccion de valores en tabla: {} - {}". format(type(e).__name__, e))
+                mensajes.append("Error en acceso a la base de datos. Consulte con el administrador.")
+                return render_template ('index', mensajes=mensajes)           
         else:
-            validar_monedas = monedas_inicio()
-            validacion = Validacion()
+            try: 
+                validar_monedas = monedas_inicio()
+            except Exception as e:
+                print("**ERROR**: Acceso a la base de datos - monedas disponibles: {} - {}". format(type(e).__name__, e))
+                mensajes.append("Error en acceso a la base de datos. Consulte con el administrador.")
+                return render_template ('compra_criptos.html', validacion=validacion, validar_monedas = [], mensajes=mensajes )
+            #validacion = Validacion()
             validacion.from_currency.choices = validar_monedas
             vacio=True
             return render_template ('compra_criptos.html', validacion=validacion, validar_monedas = validar_monedas, vacio=vacio)
                               
 @app.route("/status")
 def estado_inversiones():
+    mensajes = []
     if request.method == "GET":
-        hucha = monedas_invertidas()
+        try:
+            hucha = monedas_invertidas()
+        except Exception as e:
+            print("**ERROR**: Acceso a la base de datos - monedas inicio disponibles: {} - {}". format(type(e).__name__, e))
+            mensajes.append("Error en acceso a la base de datos. Consulte con el administrador.")
+            return render_template('estado_inversion.html', suma_saldo=[], suma_cotizacion=[], suma_invertidos=[], valor_actual=[], rentabilidad=[], mensajes=mensajes)
         monedas_saldo = {}
         for moneda in hucha:
             if hucha[moneda] > 0:
@@ -112,10 +153,6 @@ def estado_inversiones():
         suma_saldo = -300000
         for numero in euros_saldo:
             suma_saldo += numero
-                
-        print ("SOY SALDO EN €: ", suma_saldo)        
-        
-        print (monedas_saldo) #INVERSION EN DIFERENTES CRIPTOMONEDAS
         
         #PASAMOS LAS CRIPTOMONEDAS A SU VALOR ACTUAL EN €
         monedas_consulta = list(monedas_saldo.items())
@@ -127,8 +164,12 @@ def estado_inversiones():
             
             if symbol != "EUR":
                 url = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount={}&symbol={}&convert={}&CMC_PRO_API_KEY={}'.format(amount,symbol,convert,API_KEY)
-                
-                respuesta = consulta_api(url)
+                try: 
+                    respuesta = consulta_api(url) #Llamamos a la función de consuta de la API
+                except Exception as e:
+                    print("**ERROR**: Acceso a la API - intercambio de precio de las monedas: {} - {}". format(type(e).__name__, e))
+                    mensajes.append("Error en acceso a la API. Consulte con el administrador.")
+                    return render_template('estado_inversion.html', suma_saldo=[], suma_cotizacion=[], suma_invertidos=[], valor_actual=[], rentabilidad=[], mensajes=mensajes)
                                                 
                 to_quantity = respuesta['data']['quote'][convert]['price']
                 monedas_cotizacion.append(to_quantity)
@@ -142,7 +183,12 @@ def estado_inversiones():
         print ("SOY EL VALOR DE € EN CRIPTOS: ", suma_cotizacion) # SUMA DEL VALOR DE LAS CRIPTOS EN EUROS
                                          
         # CALCULAMOS EL TOTAL DE EUROS INVERTIDOS
-        hucha_euro = euros_invertidos()
+        try: 
+            hucha_euro = euros_invertidos()
+        except Exception as e:
+            print("**ERROR**: Acceso a la base de datos - monedas inicio disponibles: {} - {}". format(type(e).__name__, e))
+            mensajes.append("Error en acceso a la base de datos. Consulte con el administrador.")
+            return render_template('estado_inversion.html', suma_saldo=[], suma_cotizacion=[], suma_invertidos=[], valor_actual=[], rentabilidad=[], mensajes=mensajes)
         euros_saldo = {}
         euros_inversion = []
         for moneda in hucha_euro:
